c9748bba1de9a086704463fb1c74904a3d477d83
astashenkov <ya.astashenkov@gmail.com>
2023-03-26 22:16:49 +0300

    Solved Two sum problem

--- 217_contains_duplicate.py
+++ 217_contains_duplicate.py
@@ -4,6 +4,7 @@ return true if any value appears at least twice in the array,
 and return false if every element is distinct.
 """
 
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         unique = set()
